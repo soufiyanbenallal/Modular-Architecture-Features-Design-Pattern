f4d8557aa47301b60f85d9073c772b2ce5c43049
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2022-10-05 00:12:33 +0100

    feat: update hero design

--- src/app/modules/landingModule/components/heroComponent/heroComponent.tsx
+++ src/app/modules/landingModule/components/heroComponent/heroComponent.tsx
@@ -3,13 +3,17 @@ import React from 'react'
 export default function HeroComponent() {
   return (
     
-    <div className="flex relative z-20 items-center overflow-hidden h-screen">
-        <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
+    <div className="flex relative z-20 items-center overflow-hidden h-screen"
+    style={{
+        background:'url(/bg-hero.jpg)'
+    }}
+    >
+        <div className="container mx-auto px-6 flex flex-col  relative py-4">
             <div className="flex flex-col">
-                <h1 className="text-[9rem] my-6 dark:text-white uppercase tracking-widest whitespace-pre-line leading-[1]">
+                <h1 className="text-[9rem] my-6 max-w-lg dark:text-white font-mono font-bold uppercase tracking-widest whitespace-pre-line leading-[1]">
                 basic Project
                 </h1>
-                <p className="max-w-3xl text-xl md:text-xl font-bold dark:text-white text-gray-800 text-center py-2">
+                <p className="text-xl md:text-xl  text-transparent bg-clip-text bg-gradient-to-r from-secondary-3 via-secondary-2 to-secondary-1 py-2">
                     A guide demo to start you your modular project 
                 </p>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        sans: ['serif'],
+      },
+      colors:{
+        primary:{
+          1: '#17181d',
+          2: '#131318',
+          3: '#101013',
+        },
+        secondary:{
+          1: '#ffd9b0',
+          2: '#f1bc83',
+          3: '#8f6806',
+        }
+      }
+    },
   },
   plugins: [],
 }
